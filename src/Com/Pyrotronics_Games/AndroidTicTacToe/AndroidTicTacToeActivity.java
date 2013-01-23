f71806484799307de364278d6087fcc8675423de
Jesse James Lord <jesselord@pyrotronics-games.com>
2013-01-23 11:28:22 -0800

    Base project for an Eclipse Android Project 2.2

---
+++ src/Com/Pyrotronics_Games/AndroidTicTacToe/AndroidTicTacToeActivity.java
@@ -0,0 +1,13 @@
+package Com.Pyrotronics_Games.AndroidTicTacToe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidTicTacToeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
